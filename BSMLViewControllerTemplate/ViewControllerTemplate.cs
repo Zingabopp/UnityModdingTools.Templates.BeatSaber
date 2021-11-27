4557a7a169192a847a97b6de79dcd436c8e771ff
Zingabopp <Zingabopp.git.noreply@gmail.com>
2021-11-27 14:12:18 -0600

    Added item templates

---
+++ BSMLViewControllerTemplate/ViewControllerTemplate.cs
@@ -0,0 +1,33 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+using System;
+using System.Collections.Generic;
+
+
+namespace $rootnamespace$
+{
+	[HotReload(RelativePathToLayout = @"$safeitemrootname$.bsml")]
+	[ViewDefinition("$rootnamespace$.$safeitemrootname$.bsml")]
+	internal class $safeitemrootname$ : BSMLAutomaticViewController
+	{
+		private string yourTextField = "Hello World";
+        public string YourTextProperty
+        {
+            get { return yourTextField; }
+            set
+            {
+                if (yourTextField == value) return;
+                yourTextField = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [UIAction("#post-parse")]
+        internal void PostParse()
+        {
+            // Code to run after BSML finishes
+        }
+    }
+}
